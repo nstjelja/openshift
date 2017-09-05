bf5b79124c43ae27a57261c7339718a090ce4362
nikola.stjelja@pmi.com <nikola.stjelja@pmi.com>
2017-09-05 09:56:37 +0200

    Added basic index html

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+function send404Response (response){
+    ;
+    fs.createReadStream("./path/to/404.html").pipe(response);
+}
+
+function onRequest (request, response){
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.write("<html><head></head><body>heellooo</body></html>")
+    response.flushHeaders()
+    response.end()
+}
+
+http.createServer(onRequest).listen(8888);
